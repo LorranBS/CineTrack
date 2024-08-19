6cc016e946a57f4bac11f479f547f6bea9ffee20
L-Bernardes <lorran.bernardes@gsuite.iff.edu.br>
2024-08-18 22:33:06 -0300

    feat: Preparação do ambiente e estrutura básica do projeto

---
+++ src/main/java/br/edu/iff/cinetrack/controller/view/viewMainController.java
@@ -0,0 +1,17 @@
+package br.edu.iff.cinetrack.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class viewMainController {
+
+    @GetMapping("/home")
+    @ResponseBody
+    public String homeUnderConstruction() {
+        return "Página em construção! Volte em breve para conferir as novidades.";
+    }
+    
+}
